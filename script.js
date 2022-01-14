e35ab4cf145f6e6d5b14bccdf5ad056c4e03d836
VasyaPikh <pikhvasya@gmail.com>
2022-01-14 12:30:45 +0000

    Work

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function send() {
-    let x = document.getElementById("myText")
-    document.getElementById("demo").innerHTML = x.value
-    x.value = "";
-  }
+document.querySelector('button').onclick =addPost
+
+function addPost () {
+  let textMessage = document.getElementById('fan-message').value
+  let currentDate = new Date ()
+
+  if (textMessage.length < 2 || textMessage.trim () === ''){
+    alert('Введіть більше одного символу')
+  } else if 
+     (textMessage.indexOf('>') > -1 && textMessage.indexOf ('<') > -1) || textMessage.indexOf('<') === 0
+  ) { 
+  alert('Не використовуйте теги')
+  } else {
+    document.querySelector('.lastHardcore').insertAdjacentHTML(
+      'beforeend',
+      '<div class="text-content text-content-fan "><div></div> <p class="paragraph">' +
+      textMessage +
+      '</p> <div class="fotter-message flex"><div class="date">' +
+      currentDate.toLocaleString ([], {
+        dataStyle: 'short',
+        timeStyle: 'short',
+      }) +
+      '</div><div class="username">Guest</div></div></div>'
+    )
+  } 
+  document.getElementById('fan-message').value = null
+}
--- time.js
+++ time.js
@@ -1,4 +1,1 @@
-Data = new Date();
-
-var Date = new Date();
-document.getElementById('time-new').innerHTML = (`${Data.getDate()}.${Data.getMonth()+1}.${Data.getFullYear()}, ${Date.getHours()}:${Date.getMinutes()}`);
+const newComment = document
